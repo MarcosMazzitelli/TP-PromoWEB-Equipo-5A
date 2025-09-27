586e12f5c75085508348159161abce8a7580d142
MarcosMazzitelli <marcosmazzitelli89@gmail.com>
2025-09-26 22:09:27 -0300

    Clase AccesoDatos

--- datos/AccesoDatos.cs
+++ datos/AccesoDatos.cs
@@ -1,12 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace datos
 {
     internal class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+
+        }
+
+        //Constructor
+        public AccesoDatos()
+        {                                 //NombreServidor(el punto es genérico) ; NombreBaseDeDatos ; Forma de concetarse(Windows authentication)
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+
+        public void setearConsulta(string consulta)
+        {
+            //comando realiza la acción e inyecta la consulta SQL de tipo texto (recibido por parámetro).
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            //ejecuta el comando (consulta SQL (SELECT)) en la conexion (BD)
+            comando.Connection = conexion;
+            try
+            {
+                //se abre la conexion y se realiza la lectura.
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                //ejecuta una acción que no sea de lectura (insert, delete, update)
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int ejecutarEscalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                //ejecuta una acción que no sea de lectura (insert, delete, update)
+                return (int)comando.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            //En caso de haber un lector activo cuando se llama al método, también se cierra junto con la conexión.
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            //Seteo de parametros recibidos para ejecutar acciones en BD (NombreTabla; Valor a insertar)
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
     }
 }
