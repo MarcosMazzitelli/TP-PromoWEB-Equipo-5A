c1593f373819537ee9d37f05fe3da7688b66c989
Jerez-Bernardo-02 <jerez.bernardo.02@gmail.com>
2025-09-27 19:39:02 -0300

    Buscar - VoucherNegocio

---
+++ negocio/VoucherNegocio.cs
@@ -0,0 +1,47 @@
+﻿using datos;
+using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class VoucherNegocio
+    {
+        public Voucher buscarPorCodigo(string codigoVoucher)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT V.CodigoVoucher, V.IdCliente, V.FechaCanje, V.IdArticulo FROM Vouchers V WHERE V.CodigoVoucher = @codigoVoucher");
+                datos.setearParametro("@codigoVoucher", codigoVoucher);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    Voucher aux = new Voucher();
+
+                    aux.CodigoVoucher = (string)datos.Lector["CodigoVoucher"];
+                    aux.IdCliente = (int)datos.Lector["IdCliente"];
+                    aux.FechaCanje = (DateTime)datos.Lector["FechaCanje"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+
+                    return aux;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
